ece8be8a7fa2c386f4f03386609af08399ba283c
Subhajit Saha <103457002+atssj@users.noreply.github.com>
2025-06-12 12:18:38 +0530

    Create Subscriptions page and add upgrade prompt to Profile

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import HomePage from '@/pages/HomePage';
 import LoginPage from '@/pages/LoginPage';
 import SignupPage from '@/pages/SignupPage';
 import ProfilePage from '@/pages/ProfilePage';
+import SubscriptionsPage from '@/pages/SubscriptionsPage';
 import NotFoundPage from '@/pages/NotFoundPage';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/subscriptions" element={<SubscriptionsPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Toaster />
